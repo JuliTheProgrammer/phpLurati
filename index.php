b94b50b54a4d413bb254441b1279de2718704211
JuliTheProgrammer <juli.leipert@gmail.com>
2023-11-20 14:16:11 +0100

    finish exercise bewertung

--- index.php
+++ index.php
@@ -3,6 +3,32 @@
 $article = ["132"=>"Schuhen", "1768"=>"Jacken", "1987"=>"Hosen"];
 
 
+$bewertungen = array(
+    "Max" => array(90, 80, 70),
+    "Moritz" => array(90, 80, 70),
+    "Peter" => array(90, 80, 70),
+);
+
+//Durschnitt berechnen von den Bewertungen
+$averageOne = 0;
+$averageTwo = 0;
+$averageThree = 0;
+
+
+//build me a for loop
+foreach ($bewertungen as $b => $values) {
+    //return the average of the array
+    $averageOne += $values[0];
+    $averageTwo += $values[1];
+    $averageThree += $values[2];
+};
+
+$averageOne = $averageOne / count($bewertungen);
+$averageTwo = $averageTwo / count($bewertungen);
+$averageThree = $averageThree / count($bewertungen);
+
+//
+
 ?>
 
 <!DOCTYPE html>
@@ -21,6 +47,18 @@ $article = ["132"=>"Schuhen", "1768"=>"Jacken", "1987"=>"Hosen"];
 
         ?>
     </p>
+
+    <h3>Average</h3>
+    <ul>
+        <?php
+    
+        //For Each loop in the php block to return the average of the array
+        echo "<li>$averageOne</li>";
+        echo "<li>$averageTwo</li>";
+        echo "<li>$averageThree</li>";
+    
+        ?>
+    </ul>
     
 </body>
 </html>
